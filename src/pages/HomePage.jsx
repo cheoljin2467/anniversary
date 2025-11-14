d76aea3de01288d10f8ee792e658281165b47e18
이철진 <cheoljin@icheoljin-ui-MacBookPro.local>
2025-11-14 13:09:17 +0900

    save demo version

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,114 +4,103 @@ import Model from '../Components/Model';
 import Heart from '../assets/heart.glb';
 import Monster from'../assets/PartyMonster.glb'
 
+import StickerPeel from "../Components/StickerPeel"
+import dress from "../assets/Dress.png"
+import target1 from "../assets/target1.png"
+import target2 from "../assets/target2.png"
+
 export default function HomePage() {
   return (
 
 
-    <div className="bg-white text-black min-h-screen relative">
-      {/* 🎬 2️⃣ 중앙 sticky scroll scene */}
+    <div className="bg-white text-black min-h-screen font-soria relative">
+
+      {/*--------- 입장 화면 ---------*/}
+      <section className="relative min-h-[600vh]">
+
+        {/* sticky center */}
+        <div className="sticky top-1/2 h-screen">
+          <h1 className="text-9xl text-center">Welcome</h1>
+        </div>
+
+        {/* 스크롤되는 배경 콘텐츠 */}
+        <div className="h-[200vh] text-center">1-1</div>
+        <div className="h-[200vh] text-center">1-2</div>
+        <div className="h-[200vh] bg-[black] text-center">1-3</div>
+      </section>
+
+
+      {/*--------- After 2022.11.20 ---------*/}
       <section className="relative min-h-[600vh]">
         {/* sticky center */}
-        <div className="sticky top-0 h-screen flex items-center justify-center bg-[yello]">
-          <div className="mt-14 w-full bg-[yellow]">
-            <h1 className="text-5xl font-kor text-center">AAAAAAAAAAAAAAAAA</h1>
-            <h1 className="mt-5 text-5xl font-kor text-left">BBBBBBBBBBB</h1>
-            <h1 className="text-5xl font-kor text-right">BCDEF</h1>
-            <p className="text-lg text-gray-600 text-center">여기에 뭐쓰지</p>
-          </div>
+        <div className="sticky top-1/2 h-screen">
+          <h1 className="text-9xl text-center">After 2022.11.20</h1>
         </div>
 
         {/* 스크롤되는 배경 콘텐츠 */}
-        <div className="w-full absolute flex-col inset-0 h-[100vh]">
-          <div className="relative flex">
-               <div className="w-[20vw] flex flex-col border">
-                <Model
-                  url={Monster}
-                  height="30vh"
-                  width="20vw"
-                  autoRotate
-                  fadeIn
-                  showScreenshotButton={false}
-                  defaultZoom={4}         // 👈 더 멀리 보기
-                  minZoomDistance={4}     // 👈 너무 가까워지는 것 방지
-                  maxZoomDistance={8}
-                />
-                <p className="font-eng font-bold text-center"> Turn Me! </p>
-            </div>
-            <div className="mt-5 whitespace-pre-line text-4xl font-kor">{"Top-Right"}</div>
-            <p className="mt-5 ml-auto whitespace-pre-line text-4xl font-kor">{"TOOOOOP\n RightTTTT"}</p>  
-          </div>
-          <div className="mt-5 whitespace-pre-line text-right text-4xl font-kor">
-            {"Side\nSIde"}
-          </div>
-          <div className="w-[20vw] flex ml-auto flex-col border">
-                <Model
-                  url={Monster}
-                  height="30vh"
-                  width="20vw"
-                  autoRotate
-                  fadeIn
-                  showScreenshotButton={false}
-                  defaultZoom={4}         // 👈 더 멀리 보기
-                  minZoomDistance={4}     // 👈 너무 가까워지는 것 방지
-                  maxZoomDistance={8}
-                />
-                <p className="font-eng font-bold text-center"> Turn Me! </p>
-            </div>
-            
-            
-        </div>
+        <div className="h-[100vh] text-center">2-1</div>
+        <div className="h-[200vh] text-center">2-2</div>
+        <div className="h-[200vh] bg-[black] text-center">2-3</div>
       </section>
-         <section className="relative min-h-[600vh]">
+
+        {/*--------- After D+365 ---------*/}
+      <section className="relative min-h-[600vh]">
         {/* sticky center */}
-        <div className="sticky top-0 h-screen flex items-center justify-center bg-[yello]">
-          <div className="mt-14 w-full bg-[yellow]">
-            <h1 className="text-5xl font-kor text-center">AAAAAAAAAAAAAAAAA</h1>
-            <h1 className="mt-5 text-5xl font-kor text-left">BBBBBBBBBBB</h1>
-            <h1 className="text-5xl font-kor text-right">BCDEF</h1>
-            <p className="text-lg text-gray-600 text-center">여기에 뭐쓰지</p>
-          </div>
+        <div className="sticky top-1/2 h-screen">
+          <h1 className="text-9xl text-center">After 2023.11.20</h1>
         </div>
 
         {/* 스크롤되는 배경 콘텐츠 */}
-        <div className="w-full absolute flex-col inset-0 h-[100vh]">
-          <div className="relative flex">
-               <div className="w-[20vw] flex flex-col border">
-                <Model
-                  url={Monster}
-                  height="30vh"
-                  width="20vw"
-                  autoRotate
-                  fadeIn
-                  showScreenshotButton={false}
-                  defaultZoom={4}         // 👈 더 멀리 보기
-                  minZoomDistance={4}     // 👈 너무 가까워지는 것 방지
-                  maxZoomDistance={8}
-                />
-                <p className="font-eng font-bold text-center"> Turn Me! </p>
-            </div>
-            <div className="mt-5 whitespace-pre-line text-4xl font-kor">{"Top-Right"}</div>
-            <p className="mt-5 ml-auto whitespace-pre-line text-4xl font-kor">{"TOOOOOP\n RightTTTT"}</p>  
-          </div>
-          <div className="mt-5 whitespace-pre-line text-right text-4xl font-kor">
-            {"Side\nSIde"}
-          </div>
-          <div className="w-[20vw] flex ml-auto flex-col border">
-                <Model
-                  url={Monster}
-                  height="30vh"
-                  width="20vw"
-                  autoRotate
-                  fadeIn
-                  showScreenshotButton={false}
-                  defaultZoom={4}         // 👈 더 멀리 보기
-                  minZoomDistance={4}     // 👈 너무 가까워지는 것 방지
-                  maxZoomDistance={8}
-                />
-                <p className="font-eng font-bold text-center"> Turn Me! </p>
-            </div>
-            
-            
+        <div className="h-[200vh] text-center">3-1</div>
+        <div className="h-[200vh] text-center">3-2</div>
+        <div className="h-[200vh] bg-[black] text-center">3-3</div>
+      </section>
+
+        {/*--------- After D+730 ---------*/}
+      <section className="relative min-h-[600vh]">
+        {/* sticky center */}
+        <div className="sticky top-1/2 h-screen">
+          <h1 className="text-9xl text-center">After 2024.11.20</h1>
+        </div>
+
+        {/* 스크롤되는 배경 콘텐츠 */}
+        <div className="h-[200vh] text-center">4-1</div>
+        <div className="h-[200vh] text-center">4-2</div>
+        <div className="h-[200vh] bg-[black] text-center">4-3</div>
+      </section>
+
+        {/*--------- After D+1097 ---------*/}
+      <section className="relative min-h-[600vh]">
+        {/* sticky center */}
+        <div className="sticky top-1/2 h-screen">
+          <h1 className="text-9xl text-center">After 2025.11.20</h1>
+        </div>
+        {/* 스크롤되는 배경 콘텐츠 */}
+        <div className="h-[200vh] text-center">5-1</div>
+        <div className="h-[200vh] text-center">5-2</div>
+        <div className="h-[100vh] text-center bg-[red]">
+          
+          <StickerPeel
+              imageSrc={dress}
+              width={150}
+              rotate={0}
+              peelBackHoverPct={0}
+              peelBackActivePct={20}
+              shadowIntensity={0}
+              lightingIntensity={0}
+              initialPosition={{ x: 100, y: 100 }}
+          />       
+
+          <StickerPeel
+              imageSrc={target1}
+              width={150}
+              rotate={0}
+              peelBackHoverPct={0}
+              peelBackActivePct={20}
+              shadowIntensity={0}
+              lightingIntensity={0}
+              initialPosition={{ x: 200, y: 200 }}
+          /> 
         </div>
       </section>
     </div>
